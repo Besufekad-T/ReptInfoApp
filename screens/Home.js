767f3751a4cb022955019d092b47edbcdc73362e
Besufekad <besufekadtessema@gmail.com>
2024-03-08 22:45:49 -0800

    merges

--- screens/Home.js
+++ screens/Home.js
@@ -1,16 +1,32 @@
 import React from 'react';
 import { View, Text, StyleSheet, Pressable } from 'react-native';
 import COLORS from '../constants/colors';
-import HomeButton from '../components/HomeButtons';
+import PetRegButton from '../components/PetRegButtons';
 import TriviaButton from '../components/TriviaButton';
 import Divider from '../components/Divider';
+import SocialMediaButton from '../components/SocMediaButton';
 
 
 export default function Home({ navigation }) {
   return (
-    
     <View style={styles.container}>
-      <Text style={styles.text}>Home</Text>
+      {/* <Text style={styles.text}>Home</Text> */}
+      <View style={{
+        flexDirection: "row",
+        justifyContent: "center",
+        marginVertical: 22,
+        top:31
+
+        
+      }}>
+        <Pressable
+          onPress={() => navigation.navigate("petreg")}
+          style={styles.buttoncontainer}
+        >
+         <PetRegButton/>
+         
+        </Pressable>
+      </View>
       <View style={{
         flexDirection: "row",
         justifyContent: "center",
@@ -18,14 +34,26 @@ export default function Home({ navigation }) {
         
       }}>
         <Pressable
-          onPress={() => navigation.navigate("petreg")}
+          // onPress={() => navigation.navigate("SocMedia")}
+          style={styles.buttoncontainer}
+
+        >
+        <TriviaButton/>
+        </Pressable>
+        
+      </View>
+      <View style={{
+        flexDirection: "row",
+        justifyContent: "center",
+        marginVertical: 22
+        
+      }}>
+        <Pressable
+          onPress={() => navigation.navigate("SocMedia")}
           style={styles.buttoncontainer}
           
         >
-         <HomeButton/>
-         <Divider/>
-         <TriviaButton/>
-         
+        <SocialMediaButton/>
         </Pressable>
         
       </View>
@@ -36,18 +64,14 @@ export default function Home({ navigation }) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    
+    justifyContent: "center",
+    alignItems: "center",
   },
   text: {
-    fontSize: 24, 
-    fontWeight: 'bold', 
+    fontSize: 24,
+    fontWeight: "bold",
   },
-  buttoncontainer:{
-   top:-200,
-   
-
-
-  }
+  buttoncontainer: {
+    top: -200,
+  },
 });
